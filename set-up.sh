e8152c24b6b2de30c833e9d5e5989aeb84ad5be3
ugurSTG <ugur.ozdemir@stgm.com.tr>
2022-01-30 22:45:55 +0300

    set-up v2

--- set-up.sh
+++ set-up.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install python3-pip
+sudo apt-get -y install python3-pip curl
 sudo -H pip3 install --upgrade pip
 pip3 install -r requirements2.txt
 sudo apt-get update && sudo apt-get upgrade -y
+curl -sLS https://apt.adafruit.com/add | sudo bash
 sudo apt-get install xinput_calibrator
+
+sudo apt-get install ffmpeg libsm6 libxext6 libxcb-icccm4 libxcb-image0 libxcb-util1 libxcb-keysyms1 libxcb-render-util0 libxcb-xinerama0 libxcb-xkb1 libxkbcommon-x11-0 -y
