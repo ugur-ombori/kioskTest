33b1285e27380b642ef877c9be4a0058424f92d4
ugur-ombori <97463402+ugur-ombori@users.noreply.github.com>
2022-02-04 11:39:26 +0300

    Update on Printer Module
    Other printer support added.
    Printer output changed to Mac Adresses.

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /home/ombori/kioskTest/
+sudo python3 src/app.py
--- src/printerTest.py
+++ src/printerTest.py
@@ -4,6 +4,7 @@ from Ui_printerTest import Ui_Dialog
 import os
 import sys
 from escpos.connections import getUSBPrinter
+import netifaces
 
 class printerTest(QtWidgets.QDialog):
     def __init__(self, parent=None):
@@ -17,14 +18,68 @@ class printerTest(QtWidgets.QDialog):
         print(CURRENT_DIRECTORY)
         filename = os.path.join(CURRENT_DIRECTORY, "../resource/ombori_mini.jpeg")
         self.ui.label.setPixmap(QtGui.QPixmap(filename))    
+
+        self._interfaces = netifaces.interfaces()
+        try:
+            self._interfaces.remove('lo')
+        except:
+            print("no lo interface found!")
+        try:
+            self._interfaces.remove('docker0')
+        except:
+            print("no docker0 interface found!")
         
+        labelStr = ""
+        for iface in self._interfaces:
+            allAddrs = netifaces.ifaddresses(iface)
+            labelStr += 'Interface '+  iface + "\n"
+            for family in allAddrs:
+                addrs = allAddrs[family]
+                fam_name = netifaces.address_families[family]
+                for addr in addrs:
+                    if fam_name == "AF_PACKET":
+                        labelStr+='   MAC Address  :'+ addr['addr'] + "\n"
+        font2 = QtGui.QFont()
+        font2.setPointSize(10)
+        font2.setBold(False)
+        font2.setWeight(21)
+        self.ui.speed_label.setFont(font2)
+        self.ui.speed_label.setText(labelStr)
+
         self.show()
         
-    def printerPrint():
-        printer = getUSBPrinter()(idVendor=0x04e7,
-                          idProduct=0x2902, 
-                          inputEndPoint=0x81,
-                          outputEndPoint=0x01)
+    def printerPrint(self):
+        try:
+            printer = getUSBPrinter(commandSet='Generic')(idVendor=0x04b8,
+                          			      idProduct=0x0e28, 
+                          			      inputEndPoint=0x82,
+                          		   	      outputEndPoint=0x01)
+        except: 
+            print("no Epson TM-T20III printer Found!")
 
-        printer.text('AA11')
+        try:
+            printer = getUSBPrinter(commandSet='Generic')(idVendor=0x04b8,
+                          			      idProduct=0x0e1f, 
+                          			      inputEndPoint=0x82,
+                          		   	      outputEndPoint=0x01)
+        except: 
+            print("no Epson TM-m10 printer Found!")
+
+        printer.drawerKickPulse()	
+        printer.doubleHeight()
+        printer.doubleWidth()
+        printer.bold() 
+        for iface in self._interfaces:
+            allAddrs = netifaces.ifaddresses(iface)
+            printer.text('Interface %s:' % iface)
+            print('Interface %s:' % iface)
+            for family in allAddrs:
+                addrs = allAddrs[family]
+                fam_name = netifaces.address_families[family]
+                for addr in addrs:
+                    if fam_name == "AF_PACKET":
+                        printer.text('    MAC Address  : %s' % addr['addr'])
+                        print('    Address  : %s' % addr['addr'])        
         printer.lf()
+        printer.drawerKickPulse()
+        printer.cutPaper()
